ec787a1256797eba8bc0f738a539d47b31258aa7
Pieter de Buck <pwmdebuck@users.noreply.github.com>
2020-08-06 21:38:41 -0400

    cutie 3

--- Test Python Notebook.py
+++ Test Python Notebook.py
@@ -16,12 +16,6 @@ import dash_html_components as html
 import plotly.graph_objs as go
 import plotly.express as px
 
-# View all rows and columns of a dataframe
-pd.set_option('display.max_rows', None)
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', None)
-
 
 # ## Loading in Data
 
@@ -30,7 +24,6 @@ pd.set_option('display.max_colwidth', None)
 
 # Load Drivers data
 drivers_df = pd.read_csv("./f1db_csv/drivers.csv")
-drivers_df
 
 
 # In[4]:
@@ -64,7 +57,6 @@ clean_lt_df = lap_times_df[["raceId", "driverId", "lap", "milliseconds"]]
 race_1034_df = clean_lt_df[clean_lt_df.raceId == 1034]
 race_1034_df["seconds"] = race_1034_df.milliseconds / 1000
 race_1034_df = race_1034_df.drop(columns = "milliseconds")
-race_1034_df
 
 
 # In[55]:
@@ -73,7 +65,6 @@ race_1034_df
 df_1 = pd.merge(race_1034_df, drivers_df[["driverId", "driverRef", "number"]], on = "driverId")
 df_2 = pd.merge(df_1, results_1034_df[["resultId", "driverId", "constructorId"]], on = "driverId")
 df_3 = pd.merge(df_2, constructors_df[["constructorId", "constructorRef"]], on = "constructorId")
-df_3
 
 
 # In[68]:
@@ -84,13 +75,12 @@ driver_ref_table = df_3[["driverRef", "constructorRef"]].drop_duplicates()
 driver_ref_table.loc[10, :] = ["perez", "racing_point"]
 driver_ref_table = driver_ref_table.sort_values(by = "constructorRef")
 driver_ref_table = driver_ref_table.reset_index(drop = True)
-driver_ref_table
 
 
 # In[70]:
 
 
-px.line(df_3, x = "lap", y = "seconds", color = "driverRef", color_discrete_sequence = color_pal, 
+fig = px.line(df_3, x = "lap", y = "seconds", color = "driverRef", 
         hover_name = "driverRef", hover_data = {"driverRef" : False, "constructorRef" : True},
         color_discrete_map = {"hamilton" : "#00D2BE", "bottas" : "#00D2BE", 
                              "max_verstappen" : "#1E41FF", "albon" : "#1E41FF",
@@ -106,6 +96,6 @@ px.line(df_3, x = "lap", y = "seconds", color = "driverRef", color_discrete_sequ
 
 # In[ ]:
 
-
+fig.show()
 
 
